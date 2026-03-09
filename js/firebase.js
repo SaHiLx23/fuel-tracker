3db393707617a98cf6a7dcc5be80db52c71e19c5
Sahil Prajapati <102694001+SaHiLx23@users.noreply.github.com>
2026-03-09 21:48:57 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,12 +3,14 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
+
 apiKey:"AIzaSyDQu-PP2tRX1q8LQLx_HmiK6P53S7hQJhU",
 authDomain:"fuel-tracker-2baaf.firebaseapp.com",
 projectId:"fuel-tracker-2baaf",
 storageBucket:"fuel-tracker-2baaf.firebasestorage.app",
 messagingSenderId:"403213454298",
 appId:"1:403213454298:web:852d44e8febb4a975ecee3"
+
 };
 
 const app = initializeApp(firebaseConfig);
